baa7be57b16f50cd8fd9e7117c8c7801a56e1058
NiroshaVajiravelu <154041191+NiroshaVajiravelu@users.noreply.github.com>
2025-07-26 12:30:38 +0530

    Update Conversation.js

--- backend/models/Conversation.js
+++ backend/models/Conversation.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 const ConversationSchema = new mongoose.Schema({
-  userId: String,
-  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }]
+  userId: {
+    type: String,
+    required: true,
+  },
+  startedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  messages: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Message',
+  }],
 });
 
 module.exports = mongoose.model('Conversation', ConversationSchema);
